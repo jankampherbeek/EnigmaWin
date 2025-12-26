0fd372c7b4651c8c4857eb68109981f6f72ecbe8
Jan Kampherbeek <tech@kampherbeek.com>
2025-12-26 21:21:48 +0100

    Initial project

---
+++ EnigmaWin/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace EnigmaWin.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ EnigmaWin/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace EnigmaWin.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
